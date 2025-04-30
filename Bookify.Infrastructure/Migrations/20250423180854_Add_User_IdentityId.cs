887f7f6d3c3353240ad22456941e93b39f5d1efc
blekso <mihael.istvan@gmail.com>
2025-04-30 10:05:33 +0200

    init

---
+++ Bookify.Infrastructure/Migrations/20250423180854_Add_User_IdentityId.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bookify.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_User_IdentityId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "identity_id",
+                table: "users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "identity_id",
+                table: "users");
+        }
+    }
+}
---
+++ Bookify.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using Bookify.Domain.Users;
+
+namespace Bookify.Infrastructure.Repositories;
+
+internal sealed class UserRepository : Repository<User>, IUserRepository
+{
+    public UserRepository(ApplicationDbContext dbContext)
+        : base(dbContext)
+    {
+    }
+
+    public void Add(User user)
+    {
+        foreach (var role in user.Roles)
+        {
+            DbContext.Attach(role);
+        }
+
+        DbContext.Add(user);
+    }
+}
